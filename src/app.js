9e89e7ced1e93f586946adc6ba4a96d3c1332a3a
NathanPRangel <nathanpk20@gmail.com>
2023-06-25 14:48:16 -0300

    final

--- src/app.js
+++ src/app.js
@@ -1,66 +1,53 @@
-import express from "express"
-import cors from "cors"
+import express from "express";
+import cors from "cors";
 
-const app = express()
-app.use(cors())
-app.use(express.json())
+const app = express();
+app.use(cors());
+app.use(express.json());
 
-const userData = []
+const usuarios = []
+const tweets = []
 
-const userTweets = []
-
-app.post("/sign-up", (req, res) => {
-
-    const body = req.body
-
-    if (!body.username || !body.avatar) {
-        return res.status(400).send("Todos os campos são obrigatórios!")
+app.post("/sign-up", (req, res)=>{
+    const { username, avatar } = req.body
+    if(!username|| !avatar || typeof username !== "string" || typeof avatar !== "string"){
+        res.status(400).send("Todos os campos são obrigatórios!")
+        return
     }
-
-    userData.push(body)
-
+    usuarios.push(req.body)
     res.status(201).send("OK")
-
 })
 
-app.post("/tweets", (req, res) => {
-
-    const tweet = req.body.tweet
-    const user = req.headers.username
-
-    if (!user || !tweet) {
-        return res.status(400).send("Todos os campos são obrigatórios!")
+app.post("/tweets", (req,res)=>{
+    const { tweet, username } = req.body
+    const usuario = usuarios.find(usuario => usuario.username === username)
+    if(usuario === undefined){
+        res.sendStatus(401)
+        return
     }
-
-    const userExist = userData.find(data => data.username === user)
-
-    if (!userExist) {
-        return res.status(401).send("UNAUTHORIZED")
+    if(!username || !tweet || typeof username !== "string" || typeof tweet !== "string"){
+        res.status(400).send("Todos os campos são obrigatórios!")
+        return
     }
-
-    userTweets.unshift({ tweet, user })
-
-    if (userTweets.length > 10) {
-        userTweets.pop()
-    }
-
+    const avatar = usuario.avatar
+    tweets.push({username:username, avatar:avatar, tweet:tweet})
     res.status(201).send("OK")
-
 })
 
-app.get("/tweets", (req, res) => {
-
-    const sortedTweets = userTweets.map(tweet => {
-
-        const user = userData.find(item => item.username === tweet.username)
-        const list = { username: tweet.username, tweet: `${tweet.tweet}`, avatar: user.avatar}
-        return list
-
-    })
-
-    res.send(sortedTweets)
-
+app.get("/tweets", (req,res)=>{
+    const page = parseInt(req.query.page)
+    if(page === undefined || page<1){
+        res.status(400).send("Informe uma página válida!")
+        return
+    }
+    const lastTweets = tweets.slice(-10)
+    res.send(lastTweets)
 })
 
-const PORT = 5000
-app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`) )
+app.get("/tweets/:USERNAME", (req,res)=>{
+    const { USERNAME } = req.params
+    const userTweets = tweets.filter(tweet => tweet.username === USERNAME)
+    res.send(userTweets)
+})
+
+app.listen(5000);
